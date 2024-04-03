4bda16a095014cc19094bee06a9567a7bb7bf80e
Joshua Asari <josh.asari@gmail.com>
2024-04-03 17:45:28 -0400

    Finished Tour Cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import TourCard from "./components/TourCard";
+import { Container, Grid } from "@mui/material";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Container>
+                <Grid container spacing={5}>
+                    <TourCard />
+                    <TourCard />
+                    <TourCard />
+                    <TourCard />
+                </Grid>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/TourCard.jsx
@@ -0,0 +1,87 @@
+import { Grid, Paper, Typography, Box, Rating, createTheme, ThemeProvider } from "@mui/material";
+import { AccessTime } from "@mui/icons-material";
+
+const theme = createTheme({
+    components: {
+        MuiTypography: {
+            variants: [
+                {
+                    props: {
+                        variant: "body2",
+                    },
+                    style: {
+                        fontSize: 11,
+                    },
+                },
+                {
+                    props: {
+                        variant: "body3",
+                    },
+                    style: {
+                        fontSize: 9,
+                    },
+                },
+            ],
+        },
+    },
+});
+
+const TourCard = () => {
+    return (
+        <Grid item xs={3}>
+            <ThemeProvider theme={theme}>
+                <Paper elevation={3}>
+                    <img
+                        className="img"
+                        src="https://tcproduction.blob.core.windows.net/media/%7B240f8b72-1159-4fd3-a150-0a837f50ba4a%7D.2573758641_297d6d19fa_o.jpg"
+                        alt=""
+                    ></img>
+
+                    <Box paddingX={1}>
+                        <Typography variant="subtitle1" component="h2">
+                            Immerse into the Falls
+                        </Typography>
+                        <Box
+                            sx={{
+                                display: "flex",
+                                alignItems: "center",
+                            }}
+                        >
+                            <AccessTime sx={{ width: 12.5 }} />
+                            <Typography variant="body2" component="p" marginLeft={0.5}>
+                                5 hours
+                            </Typography>
+                        </Box>
+                        <Box
+                            sx={{
+                                display: "flex",
+                                alignItems: "center",
+                            }}
+                            marginTop={3}
+                        >
+                            <Rating
+                                name="read-only"
+                                value={4.5}
+                                precision={0.5}
+                                readOnly
+                                size="small"
+                            />
+                            <Typography variant="body2" component="p" marginLeft={0.5}>
+                                4.5
+                            </Typography>
+                            <Typography variant="body3" component="p" marginLeft={1.5}>
+                                (655 reviews)
+                            </Typography>
+                        </Box>
+                        <Box>
+                            <Typography variant="h6" component="h3" marginTop={0}>
+                                From $147
+                            </Typography>
+                        </Box>
+                    </Box>
+                </Paper>
+            </ThemeProvider>
+        </Grid>
+    );
+};
+export default TourCard;
